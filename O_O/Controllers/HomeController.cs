e0582650ac8a097ab42adb529964b52b3a63e7b8
Thomas Shields <thomasshields@live.com>
2011-07-05 16:48:13 -0400

    sticky footer gravatar for about page reduced text shadow

--- O_O/Controllers/HomeController.cs
+++ O_O/Controllers/HomeController.cs
@@ -17,6 +17,15 @@ namespace O_O.Controllers
 
         public ActionResult About()
         {
+            System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] bytes = System.Text.Encoding.UTF8.GetBytes("thomas@mpdteam.net");
+            bytes = md5.ComputeHash(bytes);
+            System.Text.StringBuilder s = new System.Text.StringBuilder();
+            foreach (byte b in bytes)
+            {
+                s.Append(b.ToString("x2").ToLower());
+            }
+            ViewBag.EmailHash = s;
             return View();
         }
         public ViewResult NotFound()
