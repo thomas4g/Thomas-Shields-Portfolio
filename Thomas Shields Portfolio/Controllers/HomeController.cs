95d5cbc41e4f24eaa610856b7965522dbfb81a94
Thomas Shields <thomasshields@live.com>
2012-01-27 19:54:02 -0500

    added links

--- Thomas Shields Portfolio/Controllers/HomeController.cs
+++ Thomas Shields Portfolio/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace Thomas_Shields_Portfolio.Controllers
             ViewBag.Message = "Welcome to ASP.NET MVC!";
             List<string> docs = new List<string>();
             
-            foreach (var item in Directory.GetFiles(Request.PhysicalApplicationPath + "/content/articles"))
+            foreach (var item in Directory.GetFiles(Request.PhysicalApplicationPath + "/content/article"))
             {
                 docs.Add(item.Replace('\\','/').Split('/').Last());
             }
