c675d3750eb1dee4458596c3e21b94d459bbd193
Thomas Shields <thomasshields@live.com>
2012-02-14 13:20:05 -0500

    removed register

--- Thomas Shields Portfolio/Controllers/AccountController.cs
+++ Thomas Shields Portfolio/Controllers/AccountController.cs
@@ -64,39 +64,10 @@ namespace Thomas_Shields_Portfolio.Controllers
         //
         // GET: /Account/Register
 
-        public ActionResult Register()
-        {
-            return View();
-        }
 
         //
         // POST: /Account/Register
 
-        [HttpPost]
-        public ActionResult Register(RegisterModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                // Attempt to register the user
-                MembershipCreateStatus createStatus;
-                Membership.CreateUser(model.UserName, model.Password, model.Email, null, null, true, null, out createStatus);
-
-                if (createStatus == MembershipCreateStatus.Success)
-                {
-                    FormsAuthentication.SetAuthCookie(model.UserName, false /* createPersistentCookie */);
-                    return RedirectToAction("Index", "Home");
-                }
-                else
-                {
-                    ModelState.AddModelError("", ErrorCodeToString(createStatus));
-                }
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
-        //
         // GET: /Account/ChangePassword
 
         [Authorize]
